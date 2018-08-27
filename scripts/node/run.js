090377d583a8a8901f5cb6e66806cd31bb521dca
Ken Nguyen <ikendoit1998@gmail.com>
2018-08-26 23:59:26 -0700

    node scripts: updated filter layer

--- scripts/node/run.js
+++ scripts/node/run.js
@@ -1,13 +1,41 @@
+/* 
+	author: Ken Nguyen
+*/
 const fs = require("fs");
 const pattern_1 = require('./patterns/pattern_1')
 const { bufferSplice, bufferDelete } = require('./libs/common')
 
 let hexOriginal = fs.readFileSync('vid2/vid-1.webm');
 
-for (let i of hexOriginal) {
-	if (i === 0xc3){
-		console.log('found')
-		break;
+for (let i=0; i < hexOriginal.length-1; i++) {
+
+	const curByte  		= hexOriginal[i]
+	const nextByte 		= hexOriginal[i+1]
+	let 	writeByte  	= curByte
+
+	const exists_single = pattern_1['0x'+curByte.toString(16)]
+	const exists_multi = pattern_1['0x'+curByte.toString(16)+'0x'+nextByte.toString(16)]
+
+	if (exists_single){
+		if (exists_single === 'none') {
+			hexOriginal = bufferDelete(i, i+1)
+		} else {
+			hexOriginal = bufferDelete(i,i+1)
+			hexOriginal = buffer.Splice(hexOriginal, new Buffer.from([exists_single]), i+1)
+		}
 	}
-	console.log(i)
+
+	if (exists_multi){
+		if (exists_multi === 'none') {
+			hexOriginal = bufferDelete(i, i+2)
+		} else {
+			hexOriginal = bufferDelete(i,i+2)
+			hexOriginal = buffer.Splice(hexOriginal, new Buffer.from([exists_multi]), i+1)
+		}
+	}
+
 }
+
+fs.writeFile('test-temp', hexOriginal, "hex", function(err){
+	console.log(err);
+});
